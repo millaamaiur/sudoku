ea949a6052ffb78f458a73a9c971741e063888ad
julen.corera <julen.corera@10.207.30.147>
2025-11-05 12:26:26 +0100

    prueba

--- src/VentanaAdmin.java
+++ src/VentanaAdmin.java
@@ -5,8 +5,6 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class VentanaAdmin extends JFrame {
-	String prueba="";
-	//Ya tengo acesso a las clases
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
