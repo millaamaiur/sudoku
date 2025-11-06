0fa8618cd8f4d02c13740ec6f17a7223aeec5543
julen.corera <julen.corera@10.206.181.33>
2025-11-06 09:32:51 +0100

    .

--- src/VentanaAdmin.java
+++ src/VentanaAdmin.java
@@ -8,7 +8,7 @@ public class VentanaAdmin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	//Comentario paul
+	
 	/**
 	 * Launch the application.
 	 */
