07a458f4c1bf0e3c0ec0560819b116d8047464f3
airam.fernandez <airam.fernandez@192.168.1.38>
2025-11-11 20:10:32 +0100

    Cambios JFrame

--- src/gui/VentanaAdmin.java
+++ src/gui/VentanaAdmin.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class VentanaAdmin extends JFrame {
 
@@ -32,11 +33,24 @@ public class VentanaAdmin extends JFrame {
 	 */
 	public VentanaAdmin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnAjustes = new JButton("Ajustes");
+		btnAjustes.setBounds(179, 117, 85, 21);
+		contentPane.add(btnAjustes);
+		
+		JButton btnCrearSudoku = new JButton("Crear Sudoku");
+		btnCrearSudoku.setBounds(170, 223, 110, 21);
+		contentPane.add(btnCrearSudoku);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(179, 336, 85, 21);
+		contentPane.add(btnSalir);
 	}
 
 }
--- src/gui/VentanaLogin.java
+++ src/gui/VentanaLogin.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaLogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +40,42 @@ public class VentanaLogin extends JFrame {
 	 */
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(143, 193, 45, 13);
+		contentPane.add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(248, 190, 96, 19);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblContrasenya = new JLabel("Contraseña");
+		lblContrasenya.setBounds(143, 270, 66, 13);
+		contentPane.add(lblContrasenya);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(248, 267, 96, 19);
+		contentPane.add(passwordField);
+		
+		JButton btnLogin = new JButton("Iniciar sesion");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String usuarioInput = txtUsuario.getText();
+				String contrasenyaInput = String.copyValueOf(passwordField.getPassword());
+				
+				//Los usuarios donde los vamos a crear??
+				
+			}
+		});
+		btnLogin.setBounds(163, 369, 115, 21);
+		contentPane.add(btnLogin);
 	}
-
 }
