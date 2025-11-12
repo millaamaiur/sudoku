3cd2b9885cedb49ea4b7d449449cd6f3be054811
airam.fernandez <airam.fernandez@10.207.58.208>
2025-11-12 12:59:41 +0100

    Crear ventana "Crear Sudoku" y sus respectivos botones

--- src/gui/VentanaAdmin.java
+++ src/gui/VentanaAdmin.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaAdmin extends JFrame {
 
@@ -26,15 +28,30 @@ public class VentanaAdmin extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnAjustes = new JButton("Ajustes");
-		btnAjustes.setBounds(179, 117, 85, 21);
-		contentPane.add(btnAjustes);
-		
 		JButton btnCrearSudoku = new JButton("Crear Sudoku");
+		btnCrearSudoku.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				VentanaCrearSudoku ventana = new VentanaCrearSudoku(VentanaAdmin.this);
+				ventana.setVisible(true);
+				dispose();
+			}
+		});
 		btnCrearSudoku.setBounds(170, 223, 110, 21);
 		contentPane.add(btnCrearSudoku);
 		
+		// ----- BOTON VOLVER -----
 		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				VentanaLogin ventana = new VentanaLogin();
+				ventana.setVisible(true);
+				dispose();
+				
+				
+			}
+		});
 		btnSalir.setBounds(179, 336, 85, 21);
 		contentPane.add(btnSalir);
 	}
