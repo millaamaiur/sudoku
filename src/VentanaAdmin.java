eeab5864f87c9756cf2c14e4e6dbcec963827a82
cendoya.paul <cendoya.paul@SS24-60F0FA7600>
2025-11-13 18:34:30 +0100

    botones

--- src/VentanaAdmin.java
+++ src/VentanaAdmin.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,7 +9,7 @@ public class VentanaAdmin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -35,6 +36,32 @@ public class VentanaAdmin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+	}
+	
+	private void initComponents() {
+	//botones de ventanadmin
+		botonAjustes = new JButton("Ajustes");
+		botonAjustes.setFont(new Font("Arial",Font.BOLD,16));
+		botonAjustes.setBackground(new Color(70, 130, 180));
+		botonAjustes.setForeground(Color.WHITE);
+		
+		
+		botonCrearSudoku = new JButton("Crear Sudoku");
+		botonCrearSudoku.setFont(new Font("Arial", Font.BOLD, 16));
+        botonCrearSudoku.setBackground(new Color(60, 179, 113));
+        botonCrearSudoku.setForeground(Color.WHITE);
+        
+        botonSalir = new JButton("Salir");
+        botonSalir.setFont(new Font("Arial", Font.BOLD, 16));
+        botonSalir.setBackground(new Color(205, 92, 92));
+        botonCrearSudoku.setForeground(Color.WHITE);
+        
+        contentPane.add(btnAjustes);
+        contentPane.add(btnCrearSudoku);
+        contentPane.add(btnSalir);
+        
+        configurarAcciones();
 	}
 
 }
