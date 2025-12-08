c819711684ad08e506f554a4d379a3a6525f82e2
cendoya.paul <cendoya.paul@SS24-60F0FA7600>
2025-12-08 10:28:17 +0100

    reponiendo todo

---
+++ src/gui/VentanaAjustes.java
@@ -0,0 +1,221 @@
+
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import sudokuBDFunciones.FuncionesSudoku;
+
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSlider;
+
+public class VentanaAjustes extends JFrame {
+	//Atributos de la ventana ajustes 
+	private static final long serialVersionUID = 1L;
+	private VentanaPartida parent;
+	private JPanel panelSonido;
+	private JPanel panelAtras;
+	private String dificultadOriginal;
+
+	public VentanaAjustes(VentanaPartida parent) {
+		this.parent = parent;
+		this.dificultadOriginal = parent.getDificultadActual();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		JPanel contentPane = new JPanel();
+		ImageIcon icon = new ImageIcon(getClass().getResource("/gui/logo.png"));
+		Image img = icon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+		setIconImage(img);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));		
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(4, 0, 0, 0));
+		
+		JPanel panelDif = new JPanel();
+		contentPane.add(panelDif);
+		GridBagLayout gbl_panelDif = new GridBagLayout();
+		gbl_panelDif.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panelDif.rowHeights = new int[]{0, 0};
+		gbl_panelDif.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gbl_panelDif.rowWeights = new double[]{0.0, 1.0};
+		panelDif.setLayout(gbl_panelDif);
+		
+		JLabel lblDificultad = new JLabel("Dificultad:");
+		GridBagConstraints gbc_lblDificultad = new GridBagConstraints();
+		gbc_lblDificultad.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDificultad.gridx = 3;
+		gbc_lblDificultad.gridy = 0;
+		panelDif.add(lblDificultad, gbc_lblDificultad);
+		
+		JComboBox combDificultad = new JComboBox();
+		combDificultad.setSelectedItem(dificultadOriginal);
+		combDificultad.setModel(new DefaultComboBoxModel(new String[] {"Facil", "Normal", "Dificil"}));
+		GridBagConstraints gbc_combDificultad = new GridBagConstraints();
+		gbc_combDificultad.insets = new Insets(0, 0, 0, 5);
+		gbc_combDificultad.fill = GridBagConstraints.HORIZONTAL;
+		gbc_combDificultad.gridx = 3;
+		gbc_combDificultad.gridy = 1;
+		panelDif.add(combDificultad, gbc_combDificultad);
+
+		
+		panelSonido = new JPanel();
+		contentPane.add(panelSonido);
+		GridBagLayout gbl_panelSonido = new GridBagLayout();
+		gbl_panelSonido.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0};
+		gbl_panelSonido.rowHeights = new int[]{0, 0};
+		gbl_panelSonido.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gbl_panelSonido.rowWeights = new double[]{0.0, 1.0};
+		panelSonido.setLayout(gbl_panelSonido);
+		
+		JLabel lblSonido = new JLabel("Sonido:");
+		GridBagConstraints gbc_lblSonido = new GridBagConstraints();
+		gbc_lblSonido.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSonido.gridx = 3;
+		gbc_lblSonido.gridy = 0;
+		panelSonido.add(lblSonido, gbc_lblSonido);
+		
+		JSlider slider = new JSlider();
+		GridBagConstraints gbc_slider = new GridBagConstraints();
+		gbc_slider.insets = new Insets(0, 0, 0, 5);
+		gbc_slider.gridx = 3;
+		gbc_slider.gridy = 1;
+		panelSonido.add(slider, gbc_slider);
+		slider.addChangeListener(e -> {
+		    int volumen = slider.getValue();
+		    if (parent != null) {
+		        parent.setVolumen(volumen);
+		    }
+		});
+
+		
+		
+		JPanel panelColor = new JPanel();
+		contentPane.add(panelColor);
+		GridBagLayout gbl_panelColor = new GridBagLayout();
+		gbl_panelColor.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0};
+		gbl_panelColor.rowHeights = new int[]{0, 0};
+		gbl_panelColor.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gbl_panelColor.rowWeights = new double[]{0.0, 1.0};
+		panelColor.setLayout(gbl_panelColor);
+		
+		JLabel lblColor = new JLabel("Color:");
+		GridBagConstraints gbc_lblColor = new GridBagConstraints();
+		gbc_lblColor.insets = new Insets(0, 0, 5, 5);
+		gbc_lblColor.gridx = 3;
+		gbc_lblColor.gridy = 0;
+		panelColor.add(lblColor, gbc_lblColor);
+		
+		
+		
+		
+		JButton btnColor = new JButton("Cambiar");
+		btnColor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Color nuevoColor = JColorChooser.showDialog(
+	                    null,
+	                    "Elige un color",
+	                    getContentPane().getBackground()
+	            );
+	            if (nuevoColor != null) {
+	                getContentPane().setBackground(nuevoColor);
+	                panelSonido.setBackground(nuevoColor);
+	            
+                if (parent != null) {
+                    panelColor.setBackground(nuevoColor);
+                    panelDif.setBackground(nuevoColor);
+                    panelSonido.setBackground(nuevoColor);
+                    panelAtras.setBackground(nuevoColor);
+                    
+	            }
+			}
+	      }
+		});
+		GridBagConstraints gbc_btnColor = new GridBagConstraints();
+		gbc_btnColor.insets = new Insets(0, 0, 0, 5);
+		gbc_btnColor.gridx = 3;
+		gbc_btnColor.gridy = 1;
+		panelColor.add(btnColor, gbc_btnColor);
+
+		
+		
+		panelAtras = new JPanel();
+		contentPane.add(panelAtras);
+		GridBagLayout gbl_panelAtras = new GridBagLayout();
+		gbl_panelAtras.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panelAtras.rowHeights = new int[]{0, 0};
+		gbl_panelAtras.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+		gbl_panelAtras.rowWeights = new double[]{1.0, 0.0};
+		panelAtras.setLayout(gbl_panelAtras);
+		//comentario para el push
+		Color colorPadre = parent.getContentPane().getBackground();
+		panelColor.setBackground(colorPadre);
+        panelDif.setBackground(colorPadre);
+        panelSonido.setBackground(colorPadre);
+        panelAtras.setBackground(colorPadre);
+		
+		JButton btnVolver = new JButton("Volver");
+		GridBagConstraints gbc_btnVolver = new GridBagConstraints();
+		gbc_btnVolver.insets = new Insets(0, 0, 0, 5);
+		gbc_btnVolver.gridx = 0;
+		gbc_btnVolver.gridy = 1;
+		panelAtras.add(btnVolver, gbc_btnVolver);
+		btnVolver.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+
+		        parent.setVisible(true);
+		        VentanaAjustes.this.dispose(); // cerrar sin guardar
+		    }
+		});
+
+		
+		JButton btnGuardar = new JButton("Guardar");
+		GridBagConstraints gbc_btnGuardar = new GridBagConstraints();
+		gbc_btnGuardar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnGuardar.gridx = 3;
+		gbc_btnGuardar.gridy = 1;
+		panelAtras.add(btnGuardar, gbc_btnGuardar);
+		btnGuardar.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+
+		        String dificultad = (String) combDificultad.getSelectedItem();
+		        int volumen = slider.getValue();
+		        Color colorFondo = getContentPane().getBackground();
+
+		        parent.guardarAjustes(dificultad, volumen, colorFondo);
+		        parent.setVolumen(volumen); // aplicar volumen real
+		        if (!dificultad.equals(dificultadOriginal)) {
+		            parent.crearSudoku(FuncionesSudoku.generarSudokuNuevo(dificultad));
+		        }
+		        
+		        
+		        parent.setVisible(true);
+		        VentanaAjustes.this.dispose();  
+		    }
+		});
+
+		slider.setValue(parent.getVolumenActual()); //estos dos son para el volumen
+		combDificultad.setSelectedItem(parent.getDificultadActual());
+
+	}
+
+}
---
+++ src/gui/VentanaRegristrar.java
@@ -0,0 +1,152 @@
+
+package gui;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import sudokuBDFunciones.FuncionesSudoku;
+
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+
+public class VentanaRegistrar extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private VentanaLogin parent;
+	private JTextField txtUsuario;
+	private JPasswordField passwordField;
+
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaRegistrar(VentanaLogin parent) {
+		this.parent = parent;
+		
+		ImageIcon icon = new ImageIcon(getClass().getResource("/gui/logo.png"));
+		Image img = icon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+		setIconImage(img);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//Titulo Registrar
+		JLabel lblTitulo = new JLabel("REGISTRATE");
+		lblTitulo.setBounds(180, 50, 150, 30);
+		lblTitulo.setFont(new java.awt.Font("Arial", Font.BOLD, 18));
+		contentPane.add(lblTitulo);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(220, 117, 120, 25);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(150, 120, 60, 13);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasenya = new JLabel("Contraseña");
+		lblContrasenya.setBounds(150, 160, 70, 13);
+		contentPane.add(lblContrasenya);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				parent.setVisible(true);
+				dispose();
+			}
+		});
+		btnVolver.setBounds(125, 228, 85, 21);
+		contentPane.add(btnVolver);
+		
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+								
+				String usuario = txtUsuario.getText().trim(); 
+		        String password = String.valueOf(passwordField.getPassword());
+				
+				if (usuario.isEmpty() || password.isEmpty()) {
+					
+					JOptionPane.showMessageDialog(null, "Por favor rellena los datos", "Campos vacios", JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				
+				if (Character.isDigit(usuario.charAt(0))) {
+					JOptionPane.showMessageDialog(null, 
+			                "El nombre de usuario no puede empezar por un número.", 
+			                "Formato incorrecto", 
+			                JOptionPane.WARNING_MESSAGE);
+			            return;
+				}
+				
+				if (password.length() < 8) {
+					JOptionPane.showMessageDialog(null, 
+			                "La contraseña debe tener al menos 8 caracteres.", 
+			                "Contraseña insegura", 
+			                JOptionPane.WARNING_MESSAGE);
+			            return;
+				}
+				
+				if (FuncionesSudoku.existeUsuario(usuario)) {
+		            JOptionPane.showMessageDialog(null, 
+		                "El usuario '" + usuario + "' ya esta en uso", 
+		                "Usuario Duplicado", 
+		                JOptionPane.ERROR_MESSAGE);
+		            return;
+		        }
+				
+				boolean registroExitoso = FuncionesSudoku.registrarUsuario(usuario, password);	
+				
+				
+				if (registroExitoso) {
+		            JOptionPane.showMessageDialog(null, 
+		                "¡Usuario registrado con éxito!", 
+		                "Bienvenido", 
+		                JOptionPane.INFORMATION_MESSAGE);
+		            
+		            txtUsuario.setText("");
+		            passwordField.setText("");
+		        } else {
+		            JOptionPane.showMessageDialog(null, 
+		                "Hubo un error técnico al registrarse.", 
+		                "Error BBDD", 
+		                JOptionPane.ERROR_MESSAGE);
+		        }
+			}
+		});
+		btnRegistrarse.setBounds(244, 228, 85, 21);
+		contentPane.add(btnRegistrarse);
+		
+		JLabel lblInformacion = new JLabel("Introduce tus datos");
+		lblInformacion.setBounds(190, 90, 110, 13);
+		contentPane.add(lblInformacion);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(220, 157, 120, 25);
+		contentPane.add(passwordField);
+		
+		
+	}
+}
